1c9e5f9750806b5c41cbdb239ad82a01ded3304e
Tiago S. P. Rodrigues <tiagosilvapereira3@gmail.com>
2020-03-24 19:52:52 -0300

    Running dusk only on CLI

--- tests/DuskTestCase.php
+++ tests/DuskTestCase.php
@@ -31,7 +31,7 @@ abstract class DuskTestCase extends BaseTestCase
     {
         $options = (new ChromeOptions)->addArguments([
             '--disable-gpu',
-            // '--headless',
+            '--headless',
             '--window-size=1920,1080',
         ]);
 
