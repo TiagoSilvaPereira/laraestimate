6902d5978367f4089c87a01996df2044a76875f5
Tiago S. P. Rodrigues <tiagosilvapereira3@gmail.com>
2020-03-30 17:36:41 -0300

    Changing clear estimates command to run hourly again

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command(ClearEstimates::class)->everyMinute();
+        $schedule->command(ClearEstimates::class)->hourly();
     }
 
     /**
