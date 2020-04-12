3cc39c7469e913a2b2c7d6179614785b06d3496d
Johannes <technikclou@gmail.com>
2020-04-12 11:14:27 +0200

    add German localization files

---
+++ resources/lang/de/app.php
@@ -0,0 +1,88 @@
+<?php
+
+return [
+
+    'my_estimates' => 'Meine Kostenvoranschläge',
+    'login' => 'Login',
+    'estimates' => 'Kostenvoranschläge',
+    'add_estimate' => 'Kostenvoranschlag hinzufügen',
+    'create_estimate' => 'Kostenvoranschlag erstellen',
+    'edit_estimate' => 'Kostenvoranschlag bearbeiten',
+    'no_estimates_found' => 'Keine Kostenvoranschläge gefunden',
+    'estimate_created_successfully' => 'Kostenvoranschlag erfolgreich erstellt',
+    'deleted_successfully' => 'Erfolgreich gelöscht',
+    'settings_saved_successfully' => 'Einstellungen erfolgreich gespeichert',
+    'edit_settings' => 'Einstellungen bearbeiten',
+    'settings' => 'Einstellungen',
+    'users' => 'Benutzer',
+    'estimate_content' => 'Kostenvoranschlag Inhalt',
+
+    'item_duration' => 'Position Dauer (Optional)',
+    'item_description' => 'Position Beschreibung',
+    'item_price' => 'Position Preis',
+    'use_name_as_title' => 'Name als Titel verwenden?',
+    'view_estimate' => 'Kostenvoranschlag anzeigen',
+    'currency_symbol' => 'Währungssymbol',
+    'currency_decimal_separator' => 'Dezimal-Trennzeichen',
+    'currency_thousands_separator' => 'Tausender-Trennzeichen',
+    'text_section' => 'Textabschnitt',
+    'prices_section' => 'Abschnitt Preise',
+    'all_changes_are_saved' => 'Alle Änderungen gespeichert',
+    'saving' => 'Speichere...',
+    'obligatory' => 'Obligatorisch?',
+    'add_item' => 'Position hinzufügen',
+    'add_text_section' => 'Textabschnitt hinzufügen',
+    'add_prices_section' => 'Abschnitt Preis hinzufügen',
+
+    'sending_email' => 'Versende E-Mail...',
+    'share_estimate' => 'Kostenvoranschlag teilen',
+    'copy_this_link' => 'Link kopieren',
+    'or_send_an_email' => 'Oder per E-Mail senden',
+    'type_email_address_here' => 'E-Mail-Adresse hier eingeben',
+    'description' => 'Beschreibung',
+    'duration' => 'Dauer',
+    'price' => 'Preis',
+    'section_total' => 'Abschnitt Gesamt',
+    'create_user' => 'Benutzer erstellen',
+    'edit_user' => 'Benutzer bearbeiten',
+    'user' => 'Benutzer',
+
+    'labels' => [
+        'new' => 'Neu',
+        'search' => 'Suchen...',
+        'actions' => 'Aktionen',
+        'showing' => 'Anzeigen',
+        'optional' => 'Optional',
+        'name' => 'Name',
+        'email' => 'E-Mail',
+        'password' => 'Passwort',
+        'password_confirmation' => 'Passwort bestätigen',
+        'description' => 'Beschreibung',
+        'back' => 'Zurück',
+        'create' => 'Erstellen',
+        'edit' => 'Bearbeiten',
+        'save' => 'Speichern',
+        'print' => 'Drucken',
+        'share' => 'Teilen',
+        'send' => 'Senden',
+        'total' => 'Gesamt',
+        'move' => 'Verschieben',
+        'remove' => 'Entfernen',
+        'logo' => 'Logo',
+        'show' => 'Zeigen',
+        'duplicate' => 'Kopieren',
+        'view' => 'Ansehen',
+        'back_to_index' => 'Zurück zur Übersicht',
+    ],
+
+    'dialogs' => [
+        'are_you_sure' => 'Bist du sicher?'
+    ],
+
+    'mail' => [
+        'received_estimate' => 'Sie haben einen neuen Kostenvoranschlag erhalten. Bitte klicken Sie auf die Schaltfläche unten, um den Kostenvoranschlag zu sehen:',
+        'open_estimate' => 'Kostenvoranschlag öffnen',
+        'access_using_link' => 'Oder über Link ansehen:',
+        'regards' => 'Mit freundlichen Grüßen'
+    ]
+];
---
+++ resources/lang/de/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'reset' => 'Dein Passwort wurde zurückgesetzt!',
+    'sent' => 'Wir haben dir den Link zum Zurücksetzen deines Passworts per E-Mail zugeschickt!',
+    'throttled' => 'Bitte warte, bevor du es erneut versuchst.',
+    'token' => 'Dieses Kennwortrücksetz-Token ist ungültig.',
+    'user' => "Wir können keinen Benutzer mit dieser E-Mail-Adresse finden.",
+
+];
