044e4fc6e547c5b57591a4befd40be55915f31c8
Tiago Rodrigues <tiagosilvapereira3@gmail.com>
2020-05-05 20:31:17 -0300

    Adding pt-br translations

---
+++ resources/lang/pt-br/app.php
@@ -0,0 +1,88 @@
+<?php
+
+return [
+
+    'my_estimates' => 'Meus Orçamentos',
+    'login' => 'Login',
+    'estimates' => 'Orçamentos',
+    'add_estimate' => 'Adicionar Orçamento',
+    'create_estimate' => 'Create Orçamento',
+    'edit_estimate' => 'Editar Orçamento',
+    'no_estimates_found' => 'Orçamentos não encontrados',
+    'estimate_created_successfully' => 'Orçamento criado com sucesso',
+    'deleted_successfully' => 'Deletado com sucesso',
+    'settings_saved_successfully' => 'Configurações salvas com sucesso',
+    'edit_settings' => 'Editar Configurações',
+    'settings' => 'Configurações',
+    'users' => 'Usuários',
+    'estimate_content' => 'Conteúdo do Corpo do Orçamento',
+
+    'item_duration' => 'Duração do Item (Opcional)',
+    'item_description' => 'Desc. do Item',
+    'item_price' => 'Preço do Item',
+    'use_name_as_title' => 'Usar nome como título?',
+    'view_estimate' => 'Visualizar Orçamento',
+    'currency_symbol' => 'Símbolo da Moeda',
+    'currency_decimal_separator' => 'Separador Decimal',
+    'currency_thousands_separator' => 'Separador dos Mil.',
+    'text_section' => 'Seção de Texto',
+    'prices_section' => 'Seção de Preços',
+    'all_changes_are_saved' => 'Alterações salvas',
+    'saving' => 'Salvando...',
+    'obligatory' => 'Obrigatório?',
+    'add_item' => 'Adicionar Item',
+    'add_text_section' => 'Adicionar Seção de Texto',
+    'add_prices_section' => 'Adicionar Seção de Preços',
+
+    'sending_email' => 'Enviando E-mail...',
+    'share_estimate' => 'Compartilhar Orçamento',
+    'copy_this_link' => 'Copiar Link',
+    'or_send_an_email' => 'Ou enviar um e-mail',
+    'type_email_address_here' => 'Escreva o e-mail aqui',
+    'description' => 'Descrição',
+    'duration' => 'Duração',
+    'price' => 'Preço',
+    'section_total' => 'Total da Seção',
+    'create_user' => 'Criar Usuário',
+    'edit_user' => 'Editar Usuário',
+    'user' => 'Usuário',
+
+    'labels' => [
+        'new' => 'Novo',
+        'search' => 'Pesquisar...',
+        'actions' => 'Ações',
+        'showing' => 'Mostrando',
+        'optional' => 'Opcional',
+        'name' => 'Nome',
+        'email' => 'E-mail',
+        'password' => 'Senha',
+        'password_confirmation' => 'Confirmação de Senha',
+        'description' => 'Descrição',
+        'back' => 'Voltar',
+        'create' => 'Criar',
+        'edit' => 'Editar',
+        'save' => 'Salvar',
+        'print' => 'Imprimir',
+        'share' => 'Compartilhar',
+        'send' => 'Enviar',
+        'total' => 'Total',
+        'move' => 'Mover',
+        'remove' => 'Remover',
+        'logo' => 'Imagem do Logo',
+        'show' => 'Mostrar',
+        'duplicate' => 'Duplicar',
+        'view' => 'Visualizar',
+        'back_to_index' => 'Voltar à Listagem',
+    ],
+
+    'dialogs' => [
+        'are_you_sure' => 'Tem certeza?'
+    ],
+
+    'mail' => [
+        'received_estimate' => 'Você recebeu um novo orçamento. Por favor clique no botão abaixo para visualizá-lo:',
+        'open_estimate' => 'Abrir Orçamento',
+        'access_using_link' => 'Ou acesse usando o link direto:',
+        'regards' => 'Atenciosamente'
+    ]
+];
